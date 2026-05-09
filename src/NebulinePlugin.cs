c94c9a89caa709f13d709767a5ef3aa0f6e0a8d2
ThetaHalo <theta@eps.lol>
2026-05-09 00:04:00 -0500

    Initial Commit
    Using MiraAPI.Example as a base, i guess it's time to start poking around.

---
+++ src/NebulinePlugin.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using MiraAPI.PluginLoading;
+using Reactor;
+using Reactor.Networking;
+using Reactor.Networking.Attributes;
+
+namespace MiraAPI.Example;
+
+[BepInAutoPlugin("lol.eps.nebuline", "Nebuline")]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+[BepInDependency(MiraApiPlugin.Id)]
+[ReactorModFlags(ModFlags.RequireOnAllClients)]
+public partial class NebulinePlugin : BasePlugin, IMiraPlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+    public string OptionsTitleText => "Nebuline";
+    public ConfigFile GetConfigFile() => Config;
+    public override void Load()
+    {
+        ExampleEventHandlers.Initialize();
+        Harmony.PatchAll();
+    }
+}
